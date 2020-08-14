cb87095c542f299d0ffa3b0d95f08da6f6a58060
Luis Garces <luisgarcesleon@gmail.com>
2020-08-13 21:26:34 -0300

    firts commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// required libraries
+require('rootpath')();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const router = express.Router()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(bodyParser.json())
+
+
+
+const apiPort = 4000;
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
